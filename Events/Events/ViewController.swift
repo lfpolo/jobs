b76d53b3073eb152ead8bfc1a6eb3f56f8e0a4f8
lpolo <lpolo@nelogica.com.br>
2021-01-16 21:07:51 -0300

    first commit

---
+++ Events/Events/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Events
+//
+//  Created by Luís Felipe Polo on 16/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
